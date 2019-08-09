f34f721d16bfc84b955a5cd628a7b0861b878f27
deborishig <33003016+deborishig@users.noreply.github.com>
2019-08-09 19:47:45 +0530

    none
    no need

--- Google_Translate.py
+++
@@ -1,8 +0,0 @@
-#Test to see if the translator is working
-
-from googletrans import Translator
-translator = Translator()
-translator.translate('I like eating')
-# <Translated src=ko dest=en text=Good evening. pronunciation=Good evening.>
-g=translator.translate('I like eating', dest='ne')
-print(g);
