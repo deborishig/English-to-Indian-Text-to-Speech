6285704f2967a438bb61d92a4c5e7fc6d0a7fdbd
deborishig <33003016+deborishig@users.noreply.github.com>
2019-08-09 19:46:29 +0530

    Readied for Cloud
    Set host to 0.0.0.0 so that app can run on external IP.

--- app.py
+++ app.py
@@ -60,5 +60,5 @@ def success():
 # Specifying the local host path with debugging = true
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1",port=8080,debug=True)
+    app.run(host="0.0.0.0", debug=True)
 
